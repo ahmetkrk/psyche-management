d3a7b59e49ff20d1f2ded89d602c4499914fdf9c
ahmetkrk <85306798+ahmetkrk@users.noreply.github.com>
2025-11-21 16:41:56 +0300

    Update page.tsx

--- app/studio/track/[id]/page.tsx
+++ app/studio/track/[id]/page.tsx
@@ -6,11 +6,9 @@ export async function generateStaticParams() {
 }
 
 export default function TrackPage({ params }: { params: { id: string } }) {
-  // Şimdilik sadece vol1 var
   if (params.id !== "vol1") {
     notFound();
   }
 
-  // Eski /studio/music sayfanda gördüğün tasarım
   return <StudioMusicVol1 />;
 }
